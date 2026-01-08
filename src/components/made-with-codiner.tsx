1611106d8aebce96aa8457993728cf8e65ab9d63
Subhan Haider <setupg98@gmail.com>
2026-01-07 22:10:58 -0600

    Hide MadeWithCodiner badge on smaller screens

--- src/components/made-with-codiner.tsx
+++ src/components/made-with-codiner.tsx
@@ -1,6 +1,6 @@
 export const MadeWithCodiner = () => {
   return (
-    <div className="fixed bottom-4 right-4 z-50">
+    <div className="fixed bottom-4 right-4 z-50 hidden 2xl:block">
       <a
         href="https://codiner.online"
         target="_blank"
